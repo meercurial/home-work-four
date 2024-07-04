970e2b1892edf4d780b06f72c907869594a16fe3
Alexander <iwankowi495@gmail.com>
2024-07-04 20:20:26 +0300

    first commit

---
+++ src/HW/4/Button.tsx
@@ -0,0 +1,16 @@
+type ButtonPropsType = {
+  callBack: () => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
+  name: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Button = ({ callBack, name }: ButtonPropsType) => {
+  const callBackHandler = () => {
+    callBack();
+  };
+
+  return (
+    <button id={'hw04-button'} onClick={callBackHandler}>
+      {name}
+    </button>
+  );
+};
